3ae8e95686ba43f8827f999fe9925e1e46315783
gnsqud24@naver.com <gnsqud24@naver.com>
2022-10-07 00:07:31 +0900

    Today, I learned about How to Request and Response data!!

--- app/src/main/java/org/techtown/registerloginexample/RegisterActivity.java
+++ app/src/main/java/org/techtown/registerloginexample/RegisterActivity.java
@@ -2,13 +2,75 @@ package org.techtown.registerloginexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private EditText et_id,et_pass, et_name,et_age;
+    private Button btn_register;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//액티비티 시작시 처음으로 시작되는 생명주기!
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //아이디값 찾아주기
+        et_id = findViewById(R.id.et_id);
+        et_pass = findViewById(R.id.et_pass);
+        et_name = findViewById(R.id.et_name);
+        et_age = findViewById(R.id.et_age);
+        btn_register = findViewById(R.id.btn_register);
+
+        //회원가입 버튼 클릭 시 수행
+        //회원가입을 누르면 입력된 데이터들을 주르륵 가져올 것이다.
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //EditText에 현재 입력되어있는 값을 get(가져온다) 해온다.
+                String userID = et_id.getText().toString();
+                String userPass = et_pass.getText().toString();
+                String userName = et_name.getText().toString();
+                int userAge = Integer.parseInt(et_age.getText().toString());
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //json 오브젝트를 활용해서 실제적인 회원가입 요청을 할 것이다.
+                        //json 오브젝트란? :
+                        //회원가입 요청을 한 뒤 결고값을 json 오브젝트로 받는 것
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if(success){//회원등록에 성공한 경우
+                                Toast.makeText(getApplicationContext(),"회원등록에 성공하였습니다.",Toast.LENGTH_SHORT);
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            } else{ //회원등록에 실패한 경우
+                                Toast.makeText(getApplicationContext(),"회원등록에 실패하였습니다..",Toast.LENGTH_SHORT);
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                //서버로 Volley를 이용해서 요청을 함.
+                RegisterRequest registerRequest = new RegisterRequest(userID,userPass,userName,userAge,responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+            }
+        });
     }
 }
